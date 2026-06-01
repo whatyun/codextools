ba361d119a142f78832e73889873bccc9d65e55d
heping <nihaobt@gmail.com>
2026-06-02 03:45:47 +0800

    Release CodexTools 1.1.25

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ const (
 	managerName              = "Codex++ 管理工具"
 	silentBinary             = "codextools-launcher"
 	managerBinary            = "codextools"
-	version                  = "1.1.24"
+	version                  = "1.1.25"
 	stateDirName             = ".codex-session-delete"
 	settingsFileName         = "settings.json"
 	latestStatusFileName     = "latest-status.json"
